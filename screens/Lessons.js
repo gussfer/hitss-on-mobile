4ecc26f36116f4b6d61f0692def7e86ce6d33a46
Gustavo Ferreira <oliveira.gustavo@unifesp.br>
2022-05-18 14:53:19 -0300

    add-gerate-pdf

--- screens/Lessons.js
+++ screens/Lessons.js
@@ -2,41 +2,70 @@ import { StyleSheet, View, Text, Button } from 'react-native';
 import NavBar from '../components/NavBar';
 import React from 'react';
 import api from '../services/api'
+import * as FileSystem from 'expo-file-system'
+import * as MediaLibrary from 'expo-media-library'
+import * as Sharing from 'expo-sharing'
+
 import LessonsList from '../components/LessonsList';
 import {MainContext} from "../contexts/MainContext"
 
 
 export default function Lessons({navigation, route}) {
 
-const [lessons,setLessons] = React.useState([])
-const [course,setCourse] = React.useState({})
-const [lastSeen,setLast] = React.useState(0)
-const {id_course} = route.params;
-const {userInfo, setUserInfo} = React.useContext(MainContext)
+    
+  const [lessons,setLessons] = React.useState([])
+  const [course,setCourse] = React.useState({})
+  const [lastSeen,setLast] = React.useState(0)
+  const {id_course} = route.params;
+  const {userInfo, setUserInfo} = React.useContext(MainContext)
+  const [ showMore, setShowMore ] = React.useState(false);
 
-const [ showMore, setShowMore ] = React.useState(false);
-
-const checkProgress = async (course_id, user_id) => {
-  try {
-    return await api.get(`courses/progress/${course_id}/${user_id}`)
-  } catch (error) {
-    console.log('Erro no progresso', error)
-    return error
+  const getCertificate = async () => {
+    try {
+      return await api.get(`courses/pdf/${id_course}/${userInfo.user.id_users}`, {responseType: 'blob'}).then((response) => {
+        const fr = new FileReader();
+        fr.onload = async () => {
+          const fileUri = `${FileSystem.documentDirectory}/Certificado.pdf`;
+          await FileSystem.writeAsStringAsync(fileUri, fr.result.split(",")[1], {encoding: FileSystem.EncodingType.Base64});
+          
+          if (Platform.OS === "ios") {
+            Sharing.shareAsync(fileUri);
+          } else {
+            const permission = await MediaLibrary.requestPermissionsAsync();
+            if (permission.granted) {
+              await MediaLibrary.createAssetAsync(fileUri);
+            }
+          }
+        }
+        fr.readAsDataURL(response.data);
+      })
+    } catch (error) {
+      console.log("Erro no progresso.", error);
+      return error;
+    }
   }
-}
 
-const getInfo = async (id, user_id) => {
+  const checkProgress = async (course_id, user_id) => {
+    try {
+      return await api.get(`courses/progress/${course_id}/${user_id}`)
+    } catch (error) {
+      console.log('Erro no progresso', error)
+      return error
+    }
+  }
+
+  const getInfo = async (id, user_id) => {
   const course = await api.get(`course/${id}`)
   const lessons = await api.get(`courses/list/${id}`)
   const progress = await checkProgress(id,  user_id)
   setCourse(course.data[0])
   setLessons(lessons.data)
   setLast(progress.data.lastSeen)
-}
+  }
 
-React.useEffect(() => {
-  getInfo(id_course, userInfo.user.id_users)
-}, [id_course, userInfo.user.id_users]);
+  React.useEffect(() => {
+    getInfo(id_course, userInfo.user.id_users)
+  }, [id_course, userInfo.user.id_users]);
   
   return (
     <View style={styles.container}>
@@ -50,6 +79,12 @@ React.useEffect(() => {
               </Text>
           </Text>
         </View>
+      <View style={styles.pdf}>
+        <Button 
+        color={"whitesmoke"} 
+        onPress={getCertificate} 
+        title={"Baixar certificado"}/>
+      </View>
       <View style={styles.button}>
         <Button 
           color={"#293351"} 
@@ -80,6 +115,14 @@ const styles = StyleSheet.create({
     width: 300,
     borderRadius: 5,
     marginRight: 15,
+  },
+  pdf: {
+    marginLeft: 15,
+    backgroundColor: "green",
+    width: 300,
+    borderRadius: 5,
+    marginRight: 15,
+    marginBottom: 15,
   },
   text: {
     marginTop: 10,
